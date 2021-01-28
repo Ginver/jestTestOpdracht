f8c67695801331e7c4d3549033f57327b21720ce
Ginver <74971813+Ginver@users.noreply.github.com>
2021-01-28 10:56:44 +0100

    function created

---
+++ index.js
@@ -0,0 +1,10 @@
+function multiply(num1, num2) {
+//    console.log("ik hoop dat het later duidelijker wordt");
+    console.log("INPUT:", num1, num2);
+    const result = num1 * num2;
+    console.log("Result:", result);
+    return result;
+}
+//    const result = multiply(2, 5);
+//    console.log("Uitkomst in global scope?", result);
+
